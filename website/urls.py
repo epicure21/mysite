2c9fc73c238ca831af6ae1b6547fe0a80fd06905
epicure21 <jamshidvandali75@gmail.com>
2024-12-30 20:22:52 +0330

    creared render html file
    add template

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from website.views import *
 urlpatterns = [
-    path('Home', index_view),
+    path('', index_view),
     path('about',about_view),
     path('contact', contact_view)
 ]
