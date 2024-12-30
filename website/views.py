7c2784019ce4ab1c637c5248591c3cc43ef6e940
epicure21 <jamshidvandali75@gmail.com>
2024-12-30 19:11:53 +0330

    created first app
    created first app named website

---
+++ website/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse,JsonResponse
+
+def index_view(request):
+    return HttpResponse('<h1>Home page</h1>')
+
+def contact_view(request):
+    return HttpResponse('<h1>contact page</h1>')
+
+def about_view(request):
+    return HttpResponse('<h1>about page</h1>')
