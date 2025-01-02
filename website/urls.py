87794f12ac48781c92afaff9848efaedba5c1c1c
epicure21 <jamshidvandali75@gmail.com>
2025-01-02 17:57:15 +0330

    blog page connect

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,5 @@ app_name = 'website'
 urlpatterns = [
     path('', index_view,name='index'),
     path('about',about_view, name='about'),
-    path('contact', contact_view, name='contact')
+    path('contact', contact_view, name='contact'),
 ]
